e06fe03416b31c830a2b274b424b26ceb155aa40
ahmedgalai <ahmedg369@gmail.com>
2026-02-17 03:28:41 +0100

    updates

--- g1/scripts/dev/other/basic_loco.py
+++ g1/scripts/dev/other/basic_loco.py
@@ -2,39 +2,38 @@ import time
 import sys
 import os
 sys.path.append("/home/ag/ef_ws/g1/scripts/dev")
-sys.path.insert(0, os.path.normpath(os.path.join(os.path.dirname(__file__), "..")))
-from safety.hanger_boot_sequence import hanger_boot_sequence
-iface="enp1s0"
 from ef_client import Robot
-#client = hanger_boot_sequence(iface=iface)
-
-# Walk forward ~1m (0.3 m/s x 3.3s)
-#for _ in range(10):
-#    client.Move(0.4, 0, 0)
-#client.StopMove()
-#time.sleep(0.5)
-
-# Turn ~90 deg (0.5 rad/s x 3.14s = pi/2 rad)
-#for _ in range(10):
-#    client.Move(0, 0, 0.6)
-#client.StopMove()
-#time.sleep(0.5)
-
-# Walk forward ~1m
-#for _ in range(10):
-#    client.Move(0.4, 0, 0)
-#client.StopMove()
 
 g1 = Robot("enp1s0")
+
 g1.stop()
-g1.loco_move(0.3, 0, 0)
+g1.headlight(color="red", duration=10)
+g1.say("Testing. Please be careful!")
+g1.say("running forward")
+g1.run(0.3, 0, 0)
 time.sleep(0.33)
 g1.stop()
 time.sleep(0.5)
+g1.say("turning")
 g1.loco_move(0,0,0.5)
 time.sleep(3.14)
 g1.stop()
 time.sleep(0.5)
-g1.loco_move(0.3, 0, 0)
+g1.say("walking")
+g1.walk(0.3, 0, 0)
 time.sleep(0.33)
 g1.stop()
+time.sleep(5)
+g1.say("testing arm motion, please step away")
+g1.rotate_joint("elbow",160)
+g1.rotate_joint("elbow",160,arm="left")
+g1.rotate_joint("shoulder_pitch",20)
+g1.rotate_joint("shoulder_pitch",20,arm="left")
+g1.rotate_joint("elbow", -160)
+g1.rotate_joint("elbow", -160, arm="left")
+g1.rotate_joint("shoulder_yaw", 30)
+g1.rotate_joint("shoulder_roll", 30, arm="left")
+g1.say("please put me on the support frame and hold my hands then press enter to go to damping mode")
+enterkey=input("press enter")
+#if enterkey=="":
+	#g1.damp()
