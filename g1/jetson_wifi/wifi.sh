8745d62e82a04c5b4330c35fc178c6bee00bb701
meldose <meldose@gmail.com>
2026-07-23 07:45:34 +0200

    added wifi jetson script

---
+++ g1/jetson_wifi/wifi.sh
@@ -0,0 +1,36 @@
+### STEPS FOR WIFI CONNECTION ###
+
+
+# get into this directory
+sudo vim /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+
+# add up the username and password in the file
+
+wpa_passphrase "YOUR_SSID" "YOUR_WIFI_PASSWORD" | sudo tee /etc/wpa_supplicant/wpa_supplicant-wlan0.conf >/dev/null
+
+
+# run the following command to connect to the wifi network
+
+sudo chmod 600 /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+
+# run the service to connect to the wifi network
+
+sudo systemctl daemon-reload
+sudo systemctl enable rfkill-unblock-wifi.service
+sudo systemctl enable wpa_supplicant@wlan0.service
+
+
+# after running the above commands, you can check the status of the wifi connection using the following command:
+
+sudo vim /etc/wpa_supplicant/wpa_supplicant-wlan0.conf : 
+    
+# make sure you have like this : 
+
+
+network={
+        ssid="KURZiot"
+        psk="Un1tr33-26-deSR"
+        #psk=42f3a70bed511a5e822ac5818e0481cd1d69a55a450365c2db9359e6ed3d4133
+        scan_ssid=1
+}
+
